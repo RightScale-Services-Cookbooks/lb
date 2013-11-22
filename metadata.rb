de2da5bedf3fbcf55c437582c1a723e01a7c7968
Richard Shade <rshade@rightscale.com>
2013-11-22 13:49:58 -0600

    extending metadata

--- metadata.rb
+++ metadata.rb
@@ -224,3 +224,33 @@ attribute "lb/service/account_secret",
     "lb::do_attach_request",
     "lb::do_detach_request"
   ]
+
+attribute "rsc_google_cloud/instance_id",
+  :display_namme => "RightScale Instance ID",
+  :description => "RightScale Instance ID, should be set to Env:INSTANCE_ID",
+  :required => "required",
+  :recipes => [ "rsc_google_cloud::default" ]
+
+attribute "google_cloud/lb/pool_name",
+  :display_name => "Google LB Pool Name",
+  :description => "Google LB Pool Name",
+  :required => "required",
+  :recipes => [ "rsc_google_cloud::lb_do_attach", "rsc_google_cloud::lb_do_detach" ]
+
+attribute "google_cloud/region",
+  :display_name => "Google Cloud Region",
+  :description => "Google Cloud Region",
+  :choice => [ "us-central1", "us-central2", "europe-west1" ],
+  :required => "required"
+
+attribute "google_cloud/lb/tag",
+  :display_name => "Google LB Firewall Tag",
+  :description => "Google LB Firewall Tag",
+  :required => "optional",
+  :default => "www"
+
+attribute "google_cloud/lb/ip",
+  :display_name => "Google LB IP",
+  :description => "Google Cloud Static IP",
+  :required => "required"
+
